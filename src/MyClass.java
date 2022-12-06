bbca5505cf49037e2cf0e2b00837d976099ca543
Xscape5s <95227010+Xscape5s@users.noreply.github.com>
2022-12-06 21:20:36 +0200

    Prima Versiune

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,32 @@ public class MyClass {
 
     public static void main(String[] args) {
         System.out.println("Git Test");
+        System.out.println("Test");
+        int i=0;
+        System.out.println(i);
+        /*aa
+        asa
+        as
+        sa
+        as
+        das
+        fa
+        f
+        asf
+        ag
+        as
+        gas
+        ga
+        a
+        sgasg
+        ags
+        as
+        d
+        as
+        das
+        d
+         */
+
     }
 
 }
